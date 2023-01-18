fd249b74b2536c214e54fe264308229ba8b40626
Visa Pollari <visa.pollari@gmail.com>
2023-01-18 21:29:37 +0200

    update readme

--- main.py
+++ main.py
@@ -5,89 +5,106 @@ import argparse
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from constants import BOTTOKEN
+from constants import BOTTOKEN, HOST_GROUP
 from messages import NEWCOMMENT, NEWTHREAD
+
 bot = telegram.Bot(BOTTOKEN)
 
+
 def load_chats():
-    with open('channels.json', 'r') as fp:
+    with open("channels.json", "r") as fp:
         return json.load(fp)
 
+
 def save_channels(chats):
-    with open('channels.json', 'w') as fp:
+    with open("channels.json", "w") as fp:
         chats = list(set(chats))
         json.dump(chats, fp)
+
 
 def start(update, context):
     """Send a message when the command /start is issued."""
     chats = load_chats()
-    chats.append( str( update.message.chat_id ) )
+    chats.append(str(update.message.chat_id))
     save_channels(chats)
-    update.message.reply_text('Thank you for subsciring to Kertoja!')
+    update.message.reply_text("Thank you for subsciring to Kertoja!")
+
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Risto Reipaaas!')
+    update.message.reply_text("Risto Reipaaas!")
+
 
 def users(update, context):
     """List."""
     chats = load_chats()
     names = []
-    if (update['message']['chat']['id'] == -323913787):
+    if update["message"]["chat"]["id"] == HOST_GROUP:
         for c in chats:
             try:
-                names.append('@' + (bot.get_chat_member(c, c).user.username) + ' -> ' + str(c))
+                names.append(
+                    "@" + (bot.get_chat_member(c, c).user.username) + " -> " + str(c)
+                )
             except:
                 pass
-    bot.send_message(-323913787, '\n'.join(names), parse_mode="HTML" )
+    bot.send_message(-323913787, "\n".join(names), parse_mode="HTML")
+
 
 def post(update, context):
     chats = load_chats()
     save = False
     nchats = []
-    text = update['message']['text'][5:]
-    if (update['message']['chat']['id'] == -323913787):
+    text = update["message"]["text"][5:]
+    if update["message"]["chat"]["id"] == HOST_GROUP:
         for c in chats:
             try:
-                bot.send_message(c, text, parse_mode="HTML" )
+                bot.send_message(c, text, parse_mode="HTML")
                 nchats.append(c)
             except:
                 save = True
         if save:
             save_channels(chats)
 
+
 def tell(update, context):
     """tell."""
-    if (update['message']['chat']['id'] == -323913787):
-        text = " ".join( update['message']['text'].split(" ")[2:] )
-        i = update['message']['text'].split(" ")[1]
-        bot.send_message(i, text, parse_mode="HTML" )
+    if update["message"]["chat"]["id"] == HOST_GROUP:
+        text = " ".join(update["message"]["text"].split(" ")[2:])
+        i = update["message"]["text"].split(" ")[1]
+        bot.send_message(i, text, parse_mode="HTML")
 
 
+## Sends social credit pooh sticker
 def reward(update, context):
     """rew."""
-    if (update['message']['chat']['id'] == -323913787):
-        text = update['message']['text'][8:]
+    if update["message"]["chat"]["id"] == HOST_GROUP:
+        text = update["message"]["text"][8:]
         i = bot.get_chat(text).id
-        #CAADAQADAgADf3BGHAXMZNg3IivIFgQ
-        bot.send_sticker(i, 'CAADAQADAgADf3BGHAXMZNg3IivIFgQ')
-        #bot.send_message(i, 'You have been given 20 social credits!', parse_mode="HTML" )
+        # CAADAQADAgADf3BGHAXMZNg3IivIFgQ
+        bot.send_sticker(i, "CAADAQADAgADf3BGHAXMZNg3IivIFgQ")
 
+
+## Sends social credit pooh sticker
 def punish(update, context):
     """pun."""
-    if (update['message']['chat']['id'] == -323913787):
-        text = update['message']['text'][8:]
+    if update["message"]["chat"]["id"] == HOST_GROUP:
+        text = update["message"]["text"][8:]
         i = bot.get_chat(text).id
-        bot.send_sticker(i, 'CAADAQADAwADf3BGHENZiEtY50bNFgQ')
-        #bot.send_message(i, '20 social credits have been removed!', parse_mode="HTML" )
+        bot.send_sticker(i, "CAADAQADAwADf3BGHENZiEtY50bNFgQ")
+
 
 def other(update, context):
     """other."""
-    if (update['message']['chat']['id'] != -323913787):
-        if(update['message']['sticker']):
-            bot.send_message(-323913787, 'Sticker from @' +update['message']['chat']['username'] , parse_mode="HTML" )
-        bot.forward_message(-323913787, update['message']['chat']['id'], update['message']['message_id'])
-        #bot.send_message(i, '20 social credits have been removed!', parse_mode="HTML" )
+    if update["message"]["chat"]["id"] != HOST_GROUP:
+        if update["message"]["sticker"]:
+            bot.send_message(
+                HOST_GROUP,
+                "Sticker from @" + update["message"]["chat"]["username"],
+                parse_mode="HTML",
+            )
+        bot.forward_message(
+            HOST_GROUP, update["message"]["chat"]["id"], update["message"]["message_id"]
+        )
 
 
 def tg_bot():
@@ -112,6 +129,6 @@ def tg_bot():
     updater.start_polling()
     updater.idle()
 
+
 if __name__ == "__main__":
     tg_bot()
-
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-# Lotterylunchbot
-This bot spams Tietokilta chat with new election posts
+# Vaihtobot
+
+This bot allows you to send and receive one way messages with Vaihto participants
 
 ## Setting up
+
 Use @botfather in telegram to get the bot token.  
 rename `rename_co_constants.py` to `constants.py` and place your token there.
 
 `pip install -r requirements.txt`  
 `python main.py`
+
+Put the chat id of your vaihto organizer chat to `HOST_GROUP` in `rename_co_constants.py`
